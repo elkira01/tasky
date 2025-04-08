1693a192c6772b5d8d4e977a49e2d8f20d30241b
elkira01 <elkira01@gmail.com>
2025-04-08 04:42:05 +0100

    add postcss packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,59 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginReact from 'eslint-plugin-react'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin'
+import typescriptEslintParser from '@typescript-eslint/parser'
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports'
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
+import importPlugin from 'eslint-plugin-import'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default [
+  {
+    parserOptions: {
+      sourceType: 'module'
+    },
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules', 'dist', 'build'],
+    languageOptions: {
+      parser: typescriptEslintParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        React: 'readonly',
+      },
+    },
+    plugins: {
+      'react': eslintPluginReact,
+      'prettier': eslintPluginPrettier,
+      'unusedImports': eslintPluginUnusedImports,
+      'simpleImportSort': eslintPluginSimpleImportSort,
+      '@typescript-eslint': typescriptEslintPlugin,
+      'unused-imports': eslintPluginUnusedImports,
+      'simple-import-sort': eslintPluginSimpleImportSort,
+      'import': importPlugin,
+      'react-hooks': eslintPluginReactHooks,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'off',
+      'import/no-cycle': 'error',
+    },
+  },
+]
